19476307331a311984691ae3dc57532d0ed6885b
John Freeman <jcfree@mu2edaq13.fnal.gov>
2020-03-31 17:32:05 -0500

    JCF: in dunecpplint.sh, start eliminating warnings which are only relevant to Google's style guide

--- cpplint/dunecpplint.sh
+++ cpplint/dunecpplint.sh
@@ -5,6 +5,24 @@ if [[ "$#" != "1" ]]; then
     exit 1
 fi
 
+# -build/namespaces: for source (not header) files, allow using-directives
+
+# -runtime/indentation_namespace: worry about whitespace with our formatting tools
+
+# -runtime/references: DUNE doesn't require that function arguments
+#  which can be altered need to be pointers
+
+# -runtime/string: DUNE doesn't require that static/global variables
+#  be trivially destructible
+
+# -whitespace: worry about this with our formatting tools
+
+
+header_filters="-runtime/indentation_namespace,-runtime/references,-runtime/string,-whitespace"
+source_filters="-build/namespaces,-runtime/indentation_namespace,-runtime/references,-runtime/string,-whitespace"
+
+
+
 codedir=$1
 
 if [[ ! -d $codedir ]]; then
@@ -17,7 +35,7 @@ for headerfile in $( find $codedir -name "*.hh" ); do
     echo
     echo "=========================Validating $headerfile========================="
     
-    $( dirname $0 )/dunecpplint.py --extensions=hh,cc --headers=hh --filter=-whitespace $headerfile 
+    $( dirname $0 )/dunecpplint.py --extensions=hh,cc --headers=hh --filter=$header_filters $headerfile 
 
 done
 
@@ -26,7 +44,7 @@ for sourcefile in $( find $codedir -name "*.cc" ); do
     echo
     echo "=========================Validating $sourcefile========================="
     
-    $( dirname $0 )/dunecpplint.py --extensions=hh,cc --headers=hh --filter=-whitespace $sourcefile 
+    $( dirname $0 )/dunecpplint.py --extensions=hh,cc --headers=hh --filter=$source_filters $sourcefile 
 
 done
 
