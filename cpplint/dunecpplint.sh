e41bfc54aac4221d2206882acdf45d9077f77f20
John Freeman <jcfree@mu2edaq13.fnal.gov>
2020-03-31 21:17:44 -0500

    JCF: go through the remaining entries in cpplint.py's _ERROR_CATEGORIES and in dunecpplint.sh disable the ones which are inapplicable to the DUNE C++ style guide

--- cpplint/dunecpplint.sh
+++ cpplint/dunecpplint.sh
@@ -5,9 +5,18 @@ if [[ "$#" != "1" ]]; then
     exit 1
 fi
 
+
+# -build/c++11/14 : No headers are explicitly disallowed
+
 # -build/namespaces: for source (not header) files, allow using-directives
 
 # -runtime/indentation_namespace: worry about whitespace with our formatting tools
+
+# -readability/check: described in cpplint.py as "Checks the use of
+# -CHECK and EXPECT macros" - i.e., DUNE doesn't need it as we don't
+# -use those
+
+# -readability/constructors: refers to unused Google macros
 
 # -runtime/references: DUNE doesn't require that function arguments
 #  which can be altered need to be pointers
@@ -15,12 +24,12 @@ fi
 # -runtime/string: DUNE doesn't require that static/global variables
 #  be trivially destructible
 
+# -runtime/vlog: refers to Google-specific VLOG function
+
 # -whitespace: worry about this with our formatting tools
 
-
-header_filters="-runtime/indentation_namespace,-runtime/references,-runtime/string,-whitespace"
-source_filters="-build/namespaces,-runtime/indentation_namespace,-runtime/references,-runtime/string,-whitespace"
-
+header_filters="-build/c++11,-build/c++14,-readability/check,-readability/constructors,-runtime/indentation_namespace,-runtime/references,-runtime/string,-runtime/vlog,-whitespace"
+source_filters="-build/c++11,-build/c++14,-build/namespaces,-readability/check,-readability/constructors,-runtime/indentation_namespace,-runtime/references,-runtime/string,-runtime/vlog,-whitespace"
 
 
 codedir=$1
@@ -35,7 +44,7 @@ for headerfile in $( find $codedir -name "*.hh" ); do
     echo
     echo "=========================Validating $headerfile========================="
     
-    $( dirname $0 )/dunecpplint.py --extensions=hh,cc --headers=hh --filter=$header_filters $headerfile 
+    $( dirname $0 )/dunecpplint.py --extensions=hh,cc --headers=hh --filter=${header_filters} $headerfile 
 
 done
 
@@ -44,7 +53,7 @@ for sourcefile in $( find $codedir -name "*.cc" ); do
     echo
     echo "=========================Validating $sourcefile========================="
     
-    $( dirname $0 )/dunecpplint.py --extensions=hh,cc --headers=hh --filter=$source_filters $sourcefile 
+    $( dirname $0 )/dunecpplint.py --extensions=hh,cc --headers=hh --filter=${source_filters} $sourcefile 
 
 done
 
