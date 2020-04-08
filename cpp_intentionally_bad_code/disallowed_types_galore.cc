a7812cf1d226378383249d8c0fb06759edaf3b62
John Freeman <jcfree@mu2edaq13.fnal.gov>
2020-04-08 15:04:20 -0500

    JCF: added disallowed_types_galore bad code example and demonstrated that not only dunecpplint but also duneclang-tidy (thanks to the google-runtime-int option) call ought disallowed C-style types

---
+++ cpp_intentionally_bad_code/disallowed_types_galore.cc
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+int main() {
+
+  int var1 = 1;
+  short var2 = 2;
+  long var3 = 3;
+  uint8_t var4 = 4;
+  uint64_t var5 = 5;
+
+  std::cout << var1 << " " << var2 << " " << var3 << " " << var4 << " " << var5 << std::endl;
+
+}
