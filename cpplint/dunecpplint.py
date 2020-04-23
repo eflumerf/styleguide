592f50f0b18804fac83a68277b9d58c936cff131
John Freeman <jcfree@mu2edaq13.fnal.gov>
2020-04-23 13:25:13 -0500

    JCF: make sure dunecpplint can spot a correct copyright comment even if there's an 80 character limit on a line

--- cpplint/dunecpplint.py
+++ cpplint/dunecpplint.py
@@ -1788,7 +1788,7 @@ def CheckForCopyright(filename, lines, error):
   maxline=30
   matching_lines=0
   line1="* This is part of the DUNE DAQ Application Framework, copyright 2020."
-  line2="* Licensing/copyright details are in the COPYING file that you should have received with this code."
+  line2="* Licensing/copyright details are in the COPYING file that you should" # have received with this code."
   for line in xrange(1, min(len(lines), maxline)):
     if line1 in lines[line]:
       matching_lines += 1
